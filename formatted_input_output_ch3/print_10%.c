867ff0c5272248c2b916353e3a2d667e7ac0a549
adamgeorge3300 <adamgeorge3300@gmail.com>
2020-04-02 07:10:21 -0400

    1st trial commit

---
+++ formatted_input_output_ch3/print_10%.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* This program shows how to print '%' that follows an integer '%d' */ 
+
+int main ()  {
+  
+    printf("This program shows how to print '%' that follows an integer '%d'\n");
+    printf("The net profit is %d%% \n", 10);
+    return 0;
+    }
