40f8766a6e061715d49671ce10bd518c1ed58106
adamgeorge3300 <62140410+adamgeorge3300@users.noreply.github.com>
2020-04-07 16:08:23 +0200

    Create input_with_comma.c

---
+++ formatted_input_output_ch3/input_with_comma.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/* write a program that will read an input (two numbers) with a comma
+ */
+
+int main () {
+
+    int n1, n2;
+
+    printf("Enter two numbers: ");
+    scanf("%d,%d", &n1, &n2);
+
+    printf("%d,%d", n1, n2);
+    return 0;
+}
+
+
+s
