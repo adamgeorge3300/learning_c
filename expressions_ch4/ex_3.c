941773f692cfd0f7289c24b12f0dd987f1b04350
adamgeorge3300 <adamgeorge3300@gmail.com>
2020-04-08 09:14:49 -0400

    corrected ex_3

--- expressions_ch4/ex_3.c
+++ expressions_ch4/ex_3.c
@@ -1,27 +1,17 @@
 /*
-Write a program that reads an integer entered by the user and displays it in octal (base 8):
-
-Enter a number between 0 and 32767: 1953
-In octal, your number is: 03641
-
-The output should be displayed using five digits, even if fewer digits are sufficient. Hint: To convert the number to octal, first divide it by 8; the remainder is the last digit of the octal number (1, in this case). Then divide the original number by 8 and repeat the process to arrive at the next-to-last digit.
+Rewrite the program in Programming Project 2 so that it prints the reversal of a three-digit number without using arithmeic to split the number into digits. 
 */
 
 #include <stdio.h>
 
 int main() {
-    int n;
-    
-    printf("Enter a number between 0 and 32767: "); 
-    scanf("%d", &n);
-  
-    printf("In octal, your number is: %d%d%d%d%d\n", 
-           (n/4096)%8, 
-           (n/512)%8, 
-           (n/64)%8, 
-           (n/8)%8, 
-           n%8);
-    
-  return 0;
+    int n1, n2, n3;
+
+    printf("Enter a three-digit number: ");
+    scanf("%1d%1d%1d", &n1, &n2, &n3);
+
+    printf("The reversal is: %d%d%d\n", n3, n2, n1);
+
+    return 0;
 }
 }
